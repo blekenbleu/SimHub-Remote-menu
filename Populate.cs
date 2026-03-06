2f7036e53c3dcadf41ea7c399fce850d09f0ad00
Steven Carmichael <blekenbleu@gmail.com>
2026-03-06 06:32:31 -0500

    a single loop

--- Populate.cs
+++ Populate.cs
@@ -22,24 +22,27 @@ namespace blekenbleu.SimHub_Remote_menu
 		bool Populate(List<string>props, List<string> vals, List<string> stps, int start, int stopCount)
 		{
 			bool pch = false;
-			for (int i = 0; i < props.Count; i++)
-				if (0 == props[i]?.Length)
-				{
-					props.RemoveAt(i);
-					if (i < vals.Count)
-						vals.RemoveAt(i);
-					if (i < stps.Count)
-						stps.RemoveAt(i);
-					i--;
-				}
-
 			for (int c = 0; c < props.Count; c++)
 			{
+				if (0 == props[c]?.Length)
+				{
+					props.RemoveAt(c);
+					if (c < vals.Count)
+						vals.RemoveAt(c);
+					if (c < stps.Count)
+						stps.RemoveAt(c);
+					c--;
+					pch = true;
+					continue;
+				}
+
 				// populate DisplayGrid ItemsSource
 				// WebMenu.ini contents may not match saved car properties
 				// default value from .ini
 				int index = Settings.properties.FindIndex(i => i.Name == props[c]);
 
+				if (index != c)
+					pch = true;
 				string ini = (c < vals.Count) ? vals[c] :  (0 > index) ? "0" : Settings.properties[index].Value;
 				// use Settings.properties value, if it exists, else from .ini
 				string setting = (0 > index) ? ini: Settings.properties[index].Value;
@@ -57,10 +60,9 @@ namespace blekenbleu.SimHub_Remote_menu
 			// has simValues property list changed from Settings?
 			stopCount += start;
 			if (stopCount != simValues.Count)
-				pch = set = true;
-			else for(;start < stopCount && !pch; start++)
-				if (!props.Exists(n => n == Settings.properties[start].Name))
-					pch = set = true;
+				pch = true;
+			if (pch)
+				set = true;
 			return pch;
 		}
 	}		// class WebMenu
